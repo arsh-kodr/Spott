9c48070b142df74a7397b57eb0688e0a1a0ae021
arsh-kodr <raisahab2364@gmail.com>
2025-11-28 09:28:23 +0530

    feat: add Clerk authentication and new proxy middleware
    - Updated package.json and package-lock.json to include @clerk/nextjs, @clerk/themes, convex, and react-spinners dependencies.
    - Created a new proxy.js file to implement Clerk middleware for route protection.
    - Configured protected routes for user authentication.

--- components/header.jsx
+++ components/header.jsx
@@ -1,7 +1,23 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { BarLoader } from "react-spinners";
+import { useStoreUser } from "@/hooks/use-store-user";
+import { useState } from "react";
 
 const Header = () => {
+  const { isLoading } = useStoreUser();
+  const [showUpgradeModal, setShowUpgradeModal] = useState();
   return (
     <>
       <nav className="fixed top-0 left-0 right-0  bg-background/80 backdrop-blur-xl z-20 border-b">
@@ -21,7 +37,31 @@ const Header = () => {
           {/* Search & Location - Desktop Only */}
 
           {/* Right Side Actions */}
+          <div className="flex items-center">
+                <Button variant={"ghost"} size={"sm"} onClick={setShowUpgradeModal}>Pricing</Button>
+            <Authenticated>
+              <UserButton />
+            </Authenticated>
+
+            <Unauthenticated>
+              <SignInButton mode="modal">
+                <Button size={"sm"} variant={"outline"}>
+                  Sign In
+                </Button>
+              </SignInButton>
+            </Unauthenticated>
+          </div>
         </div>
+
+        {/* Mobile Search & Location - Below Header */}
+
+        {/* Loader  */}
+
+        {isLoading && (
+          <div className="absolute bottom-0 left-0 w-full">
+            <BarLoader width={"100%"} color="#a855f7" />
+          </div>
+        )}
       </nav>
     </>
   );
